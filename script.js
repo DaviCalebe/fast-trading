79b8e5eb4fdc6df6dc13e87a89d9a4a179f59746
pedrofelipemoreira <pfelope@gmail.com>
2024-11-29 02:57:15 -0300

    tradução serviços

--- script.js
+++ script.js
@@ -29,7 +29,7 @@ updateCarousel();
 
 // ==================== Service descriptions =======================
 
-const services = {
+var services = {
   consulting: "Consultoria é o serviço oferecido por profissionais especializados que fornecem orientações e soluções personalizadas para empresas ou indivíduos em áreas específicas, como negócios, estratégia, finanças, marketing, entre outras. O objetivo é ajudar a otimizar processos, melhorar o desempenho e tomar decisões mais assertivas, levando em consideração as necessidades e objetivos do cliente.",
   import: "Importação é o processo de trazer produtos ou serviços de outro país para o mercado local. Esse processo envolve a negociação com fornecedores internacionais, o cumprimento de regulamentações legais e fiscais, e o transporte de mercadorias através das fronteiras. A importação pode ser realizada por empresas de diferentes setores que buscam acesso a novos produtos ou materiais para seus negócios.",
   export: "Exportação é a ação de enviar produtos ou serviços para outros países, visando expandir o alcance do mercado e aumentar a competitividade internacional. O processo de exportação envolve a adaptação de produtos às exigências dos mercados externos, além da documentação e logística necessárias para garantir a entrega segura e eficiente.",
@@ -100,6 +100,14 @@ const banner_button_3 = document.getElementById('banner_button_3');
 const About_title = document.getElementById('About_title');
 const text_about = document.getElementById('text_about');
 
+//services
+const title_service = document.getElementById('title_service');
+const service_consulting = document.getElementById('service_consulting');
+const service_import = document.getElementById('service_import');
+const service_export = document.getElementById('service_export');
+const service_customs = document.getElementById('service_customs');
+const service_description = document.getElementById('service_description');
+
 
 
 function toggleOptions() {
@@ -149,6 +157,22 @@ function setLanguage(language) {
 
     text_about.innerHTML = "A <strong>Fast</strong> é especializada em <strong>importação e exportação</strong>, oferecendo soluções logísticas eficientes e personalizadas. Com uma equipe qualificada, garantimos serviços <strong>agéis</strong>, <strong>seguros</strong> e realizados com <strong>transparência, experiência e agilidade</strong>, atendendo às necessidades de nossos clientes no comércio internacional. <br> Com o objetivo de ser uma referência no mercado global, a <strong>Fast</strong> constrói relações de confiança e oferece <strong>soluções logísticas</strong> inovadoras, superando as expectativas e facilitando a internacionalização dos negócios.";
 
+    //services
+    title_service.textContent = "Nossos Serviços";
+    service_consulting.textContent = "Consultoria";
+    service_import.textContent = "Importação";
+    service_export.textContent = "Exportação";
+    service_customs.textContent = "Despacho Aduaneiro";
+    service_description.textContent = "Selecione um serviço para ver a descrição.";
+
+      services = {
+      consulting: "Consultoria é o serviço oferecido por profissionais especializados que fornecem orientações e soluções personalizadas para empresas ou indivíduos em áreas específicas, como negócios, estratégia, finanças, marketing, entre outras. O objetivo é ajudar a otimizar processos, melhorar o desempenho e tomar decisões mais assertivas, levando em consideração as necessidades e objetivos do cliente.",
+      import: "Importação é o processo de trazer produtos ou serviços de outro país para o mercado local. Esse processo envolve a negociação com fornecedores internacionais, o cumprimento de regulamentações legais e fiscais, e o transporte de mercadorias através das fronteiras. A importação pode ser realizada por empresas de diferentes setores que buscam acesso a novos produtos ou materiais para seus negócios.",
+      export: "Exportação é a ação de enviar produtos ou serviços para outros países, visando expandir o alcance do mercado e aumentar a competitividade internacional. O processo de exportação envolve a adaptação de produtos às exigências dos mercados externos, além da documentação e logística necessárias para garantir a entrega segura e eficiente.",
+      customs: "Despacho aduaneiro é o conjunto de procedimentos legais e burocráticos necessários para a liberação de mercadorias nas aduanas de um país, tanto na importação quanto na exportação. Esse processo envolve a classificação fiscal das mercadorias, o pagamento de impostos e taxas, e a conformidade com as normas internacionais e locais de comércio. O despacho aduaneiro visa garantir que as mercadorias cumpram todas as exigências legais para circulação no mercado."
+    };
+
+
 
 
   } else if (language === 'English') {
@@ -175,8 +199,27 @@ function setLanguage(language) {
 
     //About
     About_title.innerHTML = "What is<br><span class=fast >FAST?</span>";
-    
+
     text_about.innerHTML = "<strong>Fast</strong> specializes in <strong>import and export</strong>, offering efficient and personalized logistics solutions. With a qualified team, we guarantee <strong>agile</strong>, <strong>safe</strong> services carried out with <strong>transparency, experience and agility</strong>, meeting the needs of our customers in international trade. <br> With the aim of being a reference in the global market, <strong>Fast</strong> builds relationships of trust and offers innovative <strong>logistics solutions</strong>, exceeding expectations and facilitating the internationalization of business.";
+
+    //services
+    title_service.textContent = "Our Services";
+    service_consulting.textContent = "Consulting";
+    service_import.textContent = "Import";
+    service_export.textContent = "Export";
+    service_customs.textContent = "Customs Clearance";
+    service_description.textContent = "Select a service to see the description.";
+
+     services = {
+
+      consulting: "Consultancy is the service offered by specialized professionals who provide personalized guidance and solutions to companies or individuals in specific areas, such as business, strategy, finance, marketing, among others. The objective is to help improve processes, improve performance and take more assertive, taking into account the client's needs and objectives.",
+
+      import: "Importation is the process of bringing products or services from another country to the local market. This process involves negotiating with international suppliers, complying with legal and tax regulations, and transporting goods across borders. Importation can be carried out by companies from different sectors seeking access to new products or materials for their businesses.",
+
+      export: "Export is the action of sending products or services to other countries, aiming to expand market reach and increase international competitiveness. The export process involves adapting products to the requirements of foreign markets, in addition to the documentation and logistics necessary to ensure safe and efficient delivery.",
+
+      customs: "Customs clearance is the set of legal and bureaucratic procedures necessary for the release of goods through a country's customs, both for import and export. This process involves the fiscal classification of goods, the payment of taxes and fees, and the compliance with international and local trade standards. Customs clearance aims to ensure that goods comply with all legal requirements for circulation in the market."
+    };
 
 
   }
